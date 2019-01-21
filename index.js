98d0bd2c1e923201c3d31c7f672ac6b3a156010f
Thiago de Sa Earp Pagliarelli <thiagoearp@gmail.com>
2019-01-21 17:39:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function fetchFiveDayForecast(city) {
 
 function displayFiveDayForecast(json) {
   //render five day forecast data to the DOM using provided IDs and json from API
-  document.querySelector('aside').innerHtml = "";
+  document.querySelector('aside').innerHTML = "";
     for (i = 0; i < json.list.length; i++) {
       const obj = json.list[i];
       const date = obj.dt_txt;
